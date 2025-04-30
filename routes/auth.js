12ced36c4875db4bc5326ce69023b3e1cbb14ac5
Harry Chew <harry@tech13.co.uk>
2025-04-30 20:20:38 +0100

    temp logging for sessions

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,8 @@ router.get('/google',
 router.get('/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
+    console.log('User:', req.user);
+    console.log('Session:', req.session);
     res.redirect('/dashboard');
   }
 );
